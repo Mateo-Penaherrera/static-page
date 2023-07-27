7399811b005f79a3467d46dd592c7885f4b693f6
Mateo-Penaherrera <mateo@gk.city>
2023-07-27 11:23:47 -0500

    VERSION ALPHA1

--- src/App.js
+++ src/App.js
@@ -1,33 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import './styles.css'; // Importa los estilos CSS
+import './style.css';
+
+const Preloader = () => {
+  return (
+    <div className="preloader">
+      {/* Agrega aquí tu animación o mensaje de carga */}
+      Cargando...
+    </div>
+  );
+};
 
 const NewsHeadlines = () => {
-  const [headlines, setHeadlines] = React.useState([]);
+  const [loading, setLoading] = useState(true);
+  const [headlines, setHeadlines] = useState([]);
 
-  React.useEffect(() => {
+  useEffect(() => {
     // Realiza la solicitud a la API de WordPress para obtener los titulares
-    axios.get('https://gk.city/wp-json/wp/v2/posts?per_page=5')
+    axios.get('https://gk.city/wp-json/wp/v2/posts?per_page=10')
       .then(response => {
         // Extrae los datos de las respuestas y actualiza el estado
         setHeadlines(response.data);
+        setLoading(false); // Indica que la carga ha terminado
       })
       .catch(error => {
         console.error('Error al obtener los titulares:', error);
+        setLoading(false); // Indica que la carga ha terminado (incluso en caso de error)
       });
   }, []);
 
   return (
     <div>
-      <center><h2>Titulares más recientes</h2></center>
-      <ul className="news-container"> {/* Agrega la clase del contenedor */}
-        {headlines.map(headline => (
-          <li key={headline.id} className="news-item"> {/* Agrega la clase del titular */}
-            <h3>{headline.title.rendered}</h3>
-            <p dangerouslySetInnerHTML={{ __html: headline.excerpt.rendered }} />
-          </li>
-        ))}
-      </ul>
+      <center><h2>Las historias mas recientes</h2></center>
+      {/* Muestra el Preloader mientras se carga la data */}
+      {loading ? (
+        <Preloader />
+      ) : (
+        <ul className="news-container">
+          {headlines.map(headline => (
+            <li key={headline.id} className="news-item">
+              <div className="content">
+                <h3>{headline.title.rendered}</h3>
+                <p dangerouslySetInnerHTML={{ __html: headline.excerpt.rendered }} />
+                <a href={headline.link} target="_blank" rel="noopener noreferrer">
+                  <button>Ver noticia</button>
+                </a>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
